c1d356a1b6b54ce4f3a44c57d0887eda62bc25ba
Jaime Fomperosa <jaime.fompe@gmail.com>
2023-12-02 12:21:28 +0100

    Day 2 2023 - Solved

---
+++ 2023/Day2/day2.py
@@ -0,0 +1,37 @@
+from re import findall
+
+
+REDS = 12
+GREENS = 13
+BLUES = 14
+
+def read_input(filename: str):
+    total = 0
+    with open(filename, 'r') as f:
+        for i,l in enumerate(f.readlines()):
+            reds_ok = all([n <= REDS for n in map(int, findall(r'(\d+) red', l))])
+            greens_ok = all([n <= GREENS for n in map(int, findall(r'(\d+) green', l))])
+            blues_ok = all([n <= BLUES for n in map(int, findall(r'(\d+) blue', l))])
+            total += (reds_ok and greens_ok and blues_ok) * (i + 1) 
+    return total
+
+
+# Part 1
+
+FILE = 'day2.txt'
+print(read_input(FILE))
+
+
+# Part 2
+
+def read_input_part2(filename: str):
+    total = 0
+    with open(filename, 'r') as f:
+        for l in f.readlines():
+            max_red = max(map(int, findall(r'(\d+) red', l)))
+            max_green = max(map(int, findall(r'(\d+) green', l)))
+            max_blue = max(map(int, findall(r'(\d+) blue', l)))
+            total += max_red * max_green * max_blue
+    return total
+
+print(read_input_part2(FILE))
