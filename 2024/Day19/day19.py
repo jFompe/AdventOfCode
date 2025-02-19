dc60b114ed453b0fc6a0b37c7402db9996b7f4ce
jFompe <jaime.fompe@gmail.com>
2025-02-20 00:00:07 +0100

    Day 19 Part 2 2024 - Solved

--- 2024/Day19/day19.py
+++ 2024/Day19/day19.py
@@ -1,14 +1,33 @@
+from functools import cache
 
 
 with open('day19.txt', 'r') as f:
-    designs = f.readline().strip().split(', ')
+    designs = set(f.readline().strip().split(', '))
     f.readline()
     models = [l.strip() for l in f.readlines()]
 
-def is_possible(model):
-    if any((des == model for des in designs)):
+def is_possible(model) -> bool:
+    if model in designs:
         return True
     prefixes = [des for des in designs if model.startswith(des)]
     return any((is_possible(model[len(pref):]) for pref in prefixes))
 
 print(sum(is_possible(model) for model in models))
+
+
+# Part 2
+
+@cache
+def possible_combinations(model) -> int:
+    a = 0
+    if not model:
+        return 0
+    if model in designs:
+        if len(model) == 1:
+            return 1
+        else:
+            a = 1
+    prefixes = [des for des in designs if model.startswith(des)]
+    return a + sum(possible_combinations(model[len(pref):]) for pref in prefixes)
+
+print(sum(possible_combinations(model) for model in models))
